e0902d0de5b6b051249390a802b4be20f3cf49b8
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-10-14 17:55:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "twilio_phone_number" "number" {
+  account_sid   = var.account_sid
   friendly_name = var.friendly_name
-  country_code  = var.country_code
   area_code     = var.area_code
 }
